52d7a7d35a5143a90b8ff733d90ab421b2842bf2
Swathi-prog <59515233+Swathi-prog@users.noreply.github.com>
2020-08-20 09:58:04 +0200

    Add files via upload

---
+++ assignmentspecs/baseAuthPage.ts
@@ -0,0 +1,14 @@
+import {browser, element, by, ExpectedConditions} from 'protractor';
+import {BasePage} from "./basePage.po";
+
+/**
+ * Class representing generic page.
+ * Methods/properties for global elements should go here.
+ *
+ * @export
+ * @class BasePage
+ */
+export class baseAuthPage extends BasePage {
+   
+
+}
